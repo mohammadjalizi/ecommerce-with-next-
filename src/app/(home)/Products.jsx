7d96514b64bcdf6d6ea82b90bb8c03a7f155cf0a
mohammad jalizi <mjalizi1376@gmail.com>
2024-08-22 10:10:06 +0330

    add card

--- src/app/(home)/Products.jsx
+++ src/app/(home)/Products.jsx
@@ -1,41 +1,57 @@
 import React from 'react'
 
-const arr = [
-    { productImg: "./images/1.png" },
-    { productImg: "./images/2.webp" },
-    { productImg: "./images/3.webp" },
-    { productImg: "./images/4.webp" },
-    { productImg: "./images/5.webp" },
-    { productImg: "./images/6.webp" },
-    { productImg: "./images/7.webp" },
-    { productImg: "./images/8.png" },
-  ];
-const Products = () => {
+// const arr = [
+//     { productImg: "./images/1.png" },
+//     { productImg: "./images/2.webp" },
+//     { productImg: "./images/3.webp" },
+//     { productImg: "./images/4.webp" },
+//     { productImg: "./images/5.webp" },
+//     { productImg: "./images/6.webp" },
+//     { productImg: "./images/7.webp" },
+//     { productImg: "./images/8.png" },
+
+
+
+
+//   ];
+
+   async function getdata() {
+    const res = await fetch('https://fakestoreapi.com/products')
+
+    
+   
+    return res.json()
+  }
+
+
+const Products =  async  () => {
+const data=await getdata()
+console.log(data)
+  
   return (
     <div>
               <section className="products flex">
-    {arr.map((item)=>{
+    {data.map((item)=>{
 
 return(
   <>
   
-  <article key={item.productImg} className="card">
+  <article title={item.title} key={item.productImg} className="card">
           <a href="/pages/product-details.html">
-            <img width="266" src={item.productImg} alt="" srcSet="" />
+            {/* <img width="266" src={item.image} alt="" srcSet="" /> */}
           </a>
 
           <div style={{width: '266px'}} className="content">
-            <h1 className="title">Product Title</h1>
+            <h1 className="title">{item.title.slice(0,20)}...</h1>
             <p className="description">
-              Lorem ipsum dolor sit amet consectetur elit adipisicing . Ex
-              tempore dolor in, accusantium laudantium accusamus.
+       {item.description.slice(0,110)}...
             </p>
 
             <div
               className="flex"
               style={{justifyContent: 'space-between', paddingBottom: '0.7rem'}}
             >
-              <div className="price">$100</div>
+              <div className="price">${item.price}</div>
 
               <button className="add-to-cart flex">
                 <i className="fa-solid fa-cart-plus"></i>
